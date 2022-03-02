1f7be537f051fd4dda74702474ad58f9213e2561
Ralf Hannuschka <r.hannuschka@gmail.com>
2022-03-02 12:26:32 +0100

    [feat: #1]: inital setup nx, docker

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'easy-bsb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
