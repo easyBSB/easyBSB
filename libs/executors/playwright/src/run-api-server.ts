317f897b667f1baec60b7944b83c40aaa9b26e17
Ralf Hannuschka (adesso) <ralf.hannuschka@adesso.de>
2022-05-03 00:47:07 +0200

    fix(playwright): api-server not closed after test finished

--- libs/executors/playwright/src/run-api-server.ts
+++ libs/executors/playwright/src/run-api-server.ts
@@ -3,11 +3,14 @@ import { ExecutorContext, runExecutor } from "@nrwl/devkit"
 export default async function* runApiServer(context: ExecutorContext) {
   const executorOptions = {
     project: "server",
-    target: "serve",
+    target: "serve-e2e",
     configuration: "development"
   }
 
-  for await (const output of await runExecutor<{ success: boolean; baseUrl?: string; }>(executorOptions, {}, context)) {
+  for await (const output of await runExecutor<{ success: boolean; path: string; }>(executorOptions, {}, context)) {
+
+    console.log(output);
+
     if (!output.success)
       throw new Error('Could not compile application files');
 
