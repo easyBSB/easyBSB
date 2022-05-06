ee37394c1f444874505b3fb762f39656a50a3a16
Ralf Hannuschka <r.hannuschka@gmail.com>
2022-05-06 15:05:41 +0200

    fix: fixed missing swagger api for auth (#69)

--- apps/server/src/app/users/dto/user.dto.ts
+++ apps/server/src/app/users/dto/user.dto.ts
@@ -1,9 +1,19 @@
-import { IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UsersDTO {
+export class UserDto {
 
+  @ApiProperty({ type: 'number' })
+  id?: number;
+
+  @ApiProperty({ type: 'string', example: 'easybsb' })
   @IsString()
   username: string
+
+  @ApiProperty({ default: false, type: "boolean" })
+  @IsBoolean()
+  @IsOptional()
+  userNeedPasswordChange?: boolean;
 
   @IsString()
   password: string
