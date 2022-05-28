f685f96a84ecf8ef6039a0e6dfc4aad1254f8e44
Ralf Hannuschka (adesso) <ralf.hannuschka@adesso.de>
2022-05-28 23:45:22 +0200

    feat(#16): fix docker after rename

--- docker/run.dev.sh
+++ docker/run.dev.sh
@@ -2,5 +2,5 @@
 
 # todo poll only for windows
 nginx
-npm start -- client --host 0.0.0.0 2>&1 & &&
+npm start -- server --host 0.0.0.0 2>&1 & &&
 npm start -- client --host 0.0.0.0 --disableHostCheck --poll 500 2>&1
