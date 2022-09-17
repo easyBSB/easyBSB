011329a1b2e936a34927eb80c4a16fb0ac1691ae
Ralf Hannuschka <r.hannuschka@gmail.com>
2022-09-17 14:05:20 +0200

    feat(#442): add validation for create

--- apps/server/src/app/bus/utils/device.validator.ts
+++ apps/server/src/app/bus/utils/device.validator.ts
@@ -1,5 +1,8 @@
+import { InjectRepository } from "@nestjs/typeorm";
 import { validate } from "class-validator";
+import { Repository } from "typeorm";
 import { ValidationErrors, ValidationResult } from "../../core/validators";
+import { Bus } from "../model/bus.entity";
 import { Device } from "../model/device.entity";
 
 /**
@@ -7,27 +10,66 @@ import { Device } from "../model/device.entity";
  */
 export class DeviceValidator {
 
-  /*
   public constructor(
-    // @InjectRepository(Device) private readonly repository: Repository<Device>
-  ) {}
-  */
+    @InjectRepository(Device) private readonly deviceRepository: Repository<Device>,
+    @InjectRepository(Bus) private readonly busRepository: Repository<Bus>
+  ) { }
 
   /**
    * @description validate bus is valid by data
    */
-  async isValid(device: Device): Promise<ValidationResult> {
-    return validate(device).then((errors): ValidationResult => {
-      if (errors.length === 0) {
-        return null;
-      }
+  async isValid(device: Device): Promise<ValidationResult | null> {
+    let validationResult: ValidationResult | null = null;
 
-      return errors.reduce<ValidationErrors>((result, current) => {
-        return {
-          ...result,
-          ...current.constraints
-        }
+    /** validate device */
+    const result = await validate(device);
+    if (result.length > 0) {
+      validationResult = result.reduce<ValidationErrors>((result, current) => {
+        return { ...result, ...current.constraints }
       }, {});
+    }
+
+    /** validate bus exists */
+    if (validationResult === null) {
+      validationResult = await this.busExists(device);
+    }
+
+    /** validate combination of address and bus_id not exists */
+    if (validationResult === null) {
+      validationResult = await this.deviceNotExists(device);
+    }
+
+    return validationResult
+  }
+
+  /**
+   * @description validate the bus exists, a devices needs a bus otherwise it
+   * will not work
+   */
+  async busExists(device: Device): Promise<ValidationResult | null> {
+    const busExists = await this.busRepository.findOneBy({ id: device.bus_id });
+    if (!busExists) {
+      return {
+        busNotExists: `Bus not found`
+      }
+    }
+    return null;
+  }
+
+  /**
+   * @description validate a given device not exists in combination with bus_id and address
+   * an bus can not have 2 devices with same address
+   */
+  async deviceNotExists(device: Device): Promise<ValidationResult | null> {
+    const deviceExists = await this.deviceRepository.findOneBy({ 
+      bus_id: device.bus_id,
+      address: device.address 
     });
+    if (deviceExists) {
+      return {
+        deviceWithAddressExists: `Device with given address on bus allready exists`
+      }
+    }
+    return null;
   }
 }
